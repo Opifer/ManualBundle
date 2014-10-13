0b37631ac80d44c9baa7567841be03288566c0c7
Tom Schillemans <t.schillemans@opifer.nl>
2014-10-13 10:09:23 +0200

    add javascript to "post" the search form to searchAction

---
+++ Resources/public/js/search.js
@@ -0,0 +1,29 @@
+/**
+ * Created by tomschillemans on 10/10/14.
+ */
+$(document).ready(function () {
+    // Listen for the form being submitted
+    $("#searchForm").submit(function () {
+        var url = $("#searchForm").attr("action"); // Get the submit url for the form
+        // Start send the post request
+        $.post(url, {
+                searchForm: $("#searchId").val(),
+                other: "attributes"
+            },
+            function (data) { // The response is in the data variable
+                if (data.responseCode === 200) {
+                    $.each(data.searchResult, function (key, value) {
+                        $('#searchResult').html(value);
+                    });
+                }
+                else if (data.responseCode === 400) // Search box empty
+                {
+                    // Set the error text if search box is empty and change the text color to be red.
+                    $('#searchResult').html(data.errorMessage);n
+                    $('#searchResult').css("color", "red");
+                }
+            }
+        );
+        return false; // We don't what the browser to submit the form
+    });
+});
