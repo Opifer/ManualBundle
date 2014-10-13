744e09b8dbbeae34eabf81879d2706e25b83fb70
Tom Schillemans <t.schillemans@opifer.nl>
2014-10-13 11:04:48 +0200

    fix loading of search.js

--- Resources/public/js/search.js
+++ Resources/public/js/search.js
@@ -2,6 +2,7 @@
  * Created by tomschillemans on 10/10/14.
  */
 $(document).ready(function () {
+    console.log("Document loaded");
     // Listen for the form being submitted
     $("#searchForm").submit(function () {
         var url = $("#searchForm").attr("action"); // Get the submit url for the form
@@ -19,7 +20,7 @@ $(document).ready(function () {
                 else if (data.responseCode === 400) // Search box empty
                 {
                     // Set the error text if search box is empty and change the text color to be red.
-                    $('#searchResult').html(data.errorMessage);n
+                    $('#searchResult').html(data.errorMessage);
                     $('#searchResult').css("color", "red");
                 }
             }
