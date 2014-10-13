8f9970b1488d77b6f98bd9e4046294274508d691
Tom Schillemans <t.schillemans@opifer.nl>
2014-10-13 16:57:44 +0200

    some reformats

--- Resources/public/js/search.js
+++ Resources/public/js/search.js
@@ -5,7 +5,7 @@ $(document).ready(function () {
     console.log("Document loaded");
     // Listen for the form being submitted
     //$("#searchForm").submit(function () {
-    $("#searchId").on("change keyup paste click", function(){
+    $("#searchId").on("change keyup paste click", function () {
         var url = $("#searchForm").attr("action"); // Get the submit url for the form
         // Start send the post request
         $.post(url, {
@@ -17,18 +17,17 @@ $(document).ready(function () {
                 $('#searchResult').empty();
 
                 if (data.responseCode === 200) {
-                        $('#searchResult').empty();
-                        $.each(eval(data.searchResult), function (key, value) {
-                            $('#searchResult').append("\
-                            <li class=\"list-group-item\">\
-                                <a href=\" "+ Routing.generate('opifer.manual.help.show', { slug: value.slug }) +" \">\
-                                   "+ value.title +"\
-                                </a>\
-                            </li>"
-                            );
-                        });
+                    $('#searchResult').empty();
+                    $.each(eval(data.searchResult), function (key, value) {
+                        $('#searchResult').append("\
+                        <li class=\"list-group-item\">\
+                            <a href=\" " + Routing.generate('opifer.manual.help.show', {slug: value.slug}) + " \">\
+                               " + value.title + "\
+                            </a>\
+                        </li>"
+                        );
+                    });
                 }
-
             }
         );
         return false; // We don't what the browser to submit the form
